d6d359bc5190dd7cdccd9b0555923ca5821bd601
Gustavo Fortunato <gustavoo.21@outlook.com>
2022-02-11 14:33:26 -0300

    Acrecentada mensagem de bom dia

--- src/application/main.java
+++ src/application/main.java
@@ -1,10 +1,22 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		
+		System.out.println("Bom dia");
+		
+		
+		
+		
+		
+		sc.close();
 	}
-
 }
